f60ecfc8feeb917db46c2f11868e6fbe53607fb4
Rizky Ramadan <ramadanrizky593@gmail.com>
2025-01-22 15:51:34 +0700

    Add Landing Page and Navbar

---
+++ src/app/Dashboard/page.tsx
@@ -0,0 +1,8 @@
+import Navbar from "@/components/ui/navbar";
+export default function Dashboard() {
+  return (
+    <div className="bg-white">
+      <Navbar></Navbar>Dashboard
+    </div>
+  );
+}
