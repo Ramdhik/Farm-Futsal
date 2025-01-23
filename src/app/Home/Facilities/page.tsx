5cdfbbe81d2e8a56ce77c2845d3f61e6dfa84051
Rizky Ramadan <ramadanrizky593@gmail.com>
2025-01-23 20:00:29 +0700

    Add About Us section and Our Facilities section on Landing Page

--- src/app/Home/Facilities/page.tsx
+++ src/app/Home/Facilities/page.tsx
@@ -1,3 +1,19 @@
-export default function Contact() {
-  return <div>Contact</div>;
-}
+const Facilities = () => {
+  return (
+    <div className="w-full h-96 bg-emerald-600 flex justify-center">
+      <div className="w-11/12 mx-auto my-auto flex justify-center py-5 px-2 text-white">
+        <div className="w-full flex justify-between">
+          <div className="w-2/6 h-72 rounded-2xl flex flex-col justify-between">
+            <h1 className="text-3xl font-bold mb-5">Our Facilities</h1>
+            <div className="flex-grow rounded-2xl bg-gray-300"></div>
+          </div>
+          <div className="flex flex-col w-3/5 ml-10">
+            <div className="flex-grow rounded-2xl bg-gray-300"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Facilities;
